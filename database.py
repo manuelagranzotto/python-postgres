82f98c3668a52979fff74f5b3baac226187721c4
unknown <manuela.granzotto.lec_a@msft.cesae.pt>
2024-05-17 15:58:52 +0100

    teste argv

--- database.py
+++ database.py
@@ -38,23 +38,5 @@ try:
     conn.close()
 except psycopg2.Error as e:
     print(f"Não foi possível conectar ao PostgreSQL {e}")
-#To see a list of all environment variables:
-# print(os.environ)
 
-# def get_vendors():
-#     """ Retrieve data from the vendors table """
-#     config  = load_config()
-#     try:
-#         with psycopg2.connect(**config) as conn:
-#             with conn.cursor() as cur:
-#                 cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
-#                 print("The number of parts: ", cur.rowcount)
-#                 row = cur.fetchone()
-
-#                 while row is not None:
-#                     print(row)
-#                     row = cur.fetchone()
-
-#     except (Exception, psycopg2.DatabaseError) as error:
-#         print(error)
 '''
