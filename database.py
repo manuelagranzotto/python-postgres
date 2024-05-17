0b7bf0c56e46b3c3fea82dedee9fd78b69fb82bd
unknown <manuela.granzotto.lec_a@msft.cesae.pt>
2024-05-17 16:00:32 +0100

    connect to pgsql

--- database.py
+++ database.py
@@ -2,22 +2,15 @@ import psycopg2
 import os
 import sys
 
-#print(os.environ)
-
-print("Argumentos:", sys.argv)
-'''
 try:
     # Connect to your postgres DB
     conn = psycopg2.connect(
-        host= sys.argv[1], #HOST_DB
+        host= sys.argv[1],
         port= int(sys.argv[2]),
         database= sys.argv[3],
         user= sys.argv[4],
         password= sys.argv[5]
     )
-    #python database.py ${{ env.HOST_DB }} ${{ secrets.HOST_PORT }} ${{ env.DATABASE }} ${{ secrets.USER_PG }} ${{ secrets.PGPASSWORD }}
-    #python database.py "host ${{ env.HOST_DB }}" "port ${{ secrets.HOST_PORT }}" "database ${{ env.DATABASE }}" "user ${{ secrets.USER_PG }}" "password ${{ secrets.PGPASSWORD }}"
-    #Argumentos: ['database.py', '***', '***', '***']
 
     # Open a cursor to perform database operations
     cur = conn.cursor()
@@ -39,4 +32,3 @@ try:
 except psycopg2.Error as e:
     print(f"Não foi possível conectar ao PostgreSQL {e}")
 
-'''
